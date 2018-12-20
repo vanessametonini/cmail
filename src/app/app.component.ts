71ff861b5a01a247f2d72cf5443d4c157843ddd7
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-12-20 20:10:18 -0200

    Ex.04 listando emails simples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  isNewEmailFormOpen = false;
+
+  private isNewEmailFormOpen = false;
+  emailList = [];
+  email = {
+    destinatario: '',
+    assunto: '',
+    conteudo: ''
+  }
 
   toggleNewEmailForm() {
     this.isNewEmailFormOpen = !this.isNewEmailFormOpen
   }
+
+  handleNewEmail(event: Event) {
+    event.preventDefault();
+
+    this.emailList.push(this.email)
+
+    this.email = {
+      destinatario: '',
+      assunto: '',
+      conteudo: ''
+    }
+
+  }
+
 }
