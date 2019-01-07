2e49d3c71ff15757ffac75c48c785e6b983f228c
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-01-07 18:33:23 -0200

    Ex.08 forms reactivos inicio

--- src/app/modules/cadastro/cadastro.component.ts
+++ src/app/modules/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
+  formCadastro = new FormGroup({
+    nome: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required]),
+    senha: new FormControl('', [Validators.required]),
+    avatar: new FormControl(),
+  })
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleCadastrarUsuario() {
+    if(this.formCadastro.valid){
+      console.log(this.formCadastro.value);
+    }
+    else {
+      console.log('Campos precisam ser preenchidos!')
+    }
   }
 
 }
