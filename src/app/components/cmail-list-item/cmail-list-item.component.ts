e2bad07ec90b74e508ea0314210d96bb1d61982b
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-01-17 14:07:40 -0200

    Ex.19 CMailListComponent

---
+++ src/app/components/cmail-list-item/cmail-list-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cmail-list-item',
+  templateUrl: './cmail-list-item.component.html',
+  styles: []
+})
+export class CmailListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
+import { CmailListItemComponent } from './cmail-list-item/cmail-list-item.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, CmailListItemComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, CmailListItemComponent]
 })
 export class SharedComponentModule{}
